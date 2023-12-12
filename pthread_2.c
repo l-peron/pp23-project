37ad7a017865fb16c3db573f1e1392e80d2cfe76
Léo Péron <peronleo8@gmail.com>
2023-12-12 09:21:11 +0100

    work in progress

--- pthread_2.c
+++ pthread_2.c
@@ -4,12 +4,20 @@
 #include <time.h>
 #include <inttypes.h>
 #include <string.h>
+#include <pthread.h>
 
 #define SIZE_X 1024
 #define SIZE_Y 1024
 #define SIZE_Z 64
 #define THRESHOLD_LIMIT 25
 #define PATTERN_SIZE 4
+#define MAX_THREAD 4
+
+pthread_t threads[MAX_THREAD];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+uint8_t*** threshold_array;
+uint64_t* reduced_array;
+uint8_t*** array;
 
 int cmpfunc (const void * a, const void * b) {
     uint64_t a_value = (uint64_t) *((uint64_t *) a);
@@ -27,13 +35,28 @@ int cmpfunc (const void * a, const void * b) {
  *  Iterate through the array, parsing (4 * 4 * 4) cubes of value of only 0 and 1
  *  And reducing them to 64 bits numbers
  */
-void reduce(uint8_t*** data, uint64_t* reduced_dst) {
-    unsigned int reduced_dst_index = 0;
+void reduce(uint8_t*** data, uint64_t* reduced_dst, int thread_id) {
+
+    const int thread_index = (int)thread_id;
+
+    const double min_born = (double)thread_index/MAX_THREAD;
+    const double max_born = (double)(thread_index+1)/MAX_THREAD;
+
+    const int start_z = min_born*SIZE_Z;
+    const int end_z = max_born*SIZE_Z;
+
+    const int start_y = min_born*SIZE_Y;
+    const int end_y = max_born*SIZE_Y;
+
+    const int start_x = min_born*SIZE_X;
+    const int end_x = max_born*SIZE_X;
+
+    unsigned int reduced_dst_index = (end_z - start_z)*(end_y - start_y)*(end_x - start_x);
 
     // Global Iterating
-    for(int z = 0; z <= SIZE_Z - PATTERN_SIZE; z += PATTERN_SIZE) {
-        for(int y = 0; y <= SIZE_Y - PATTERN_SIZE; y += PATTERN_SIZE) {
-            for(int x = 0; x <= SIZE_X - PATTERN_SIZE; x += PATTERN_SIZE) {
+    for(int z = start_z; z <= end_z - PATTERN_SIZE; z += PATTERN_SIZE) {
+        for(int y = start_y; y <= end_y - PATTERN_SIZE; y += PATTERN_SIZE) {
+            for(int x = start_x; x <= end_x - PATTERN_SIZE; x += PATTERN_SIZE) {
                 // Reduced cube
                 uint64_t threshold = 0;
                 unsigned int threshold_bit_index = 0;
@@ -108,10 +131,66 @@ unsigned int countPairs(uint64_t* data, unsigned int size) {
     return count;
 }
 
-int main() {
+void *thread_thresholding(void *i) {
+    // Start sub-timing
+    clock_t start_time = clock();
 
-    /* Initializing Threshold array */
-    uint8_t*** threshold_array;
+    const int thread_index = (int)i;
+
+    const double min_born = (double)thread_index/MAX_THREAD;
+    const double max_born = (double)(thread_index+1)/MAX_THREAD;
+
+    const int start_z = min_born*SIZE_Z;
+    const int end_z = max_born*SIZE_Z;
+
+    const int start_y = min_born*SIZE_Y;
+    const int end_y = max_born*SIZE_Y;
+
+    const int start_x = min_born*SIZE_X;
+    const int end_x = max_born*SIZE_X;
+
+	printf("* Thresholding in section: %d\n", i);
+    printf("* X Range: [%d, %d[\n", start_x, end_x);
+    printf("* Y Range: [%d, %d[\n", start_y, end_y);
+    printf("* Z Range: [%d, %d[\n", start_z, end_z);
+
+    // Thresholding voxels
+    for (int z = start_z; z < start_z; ++z) {
+        for (int y = start_y; y < SIZE_Y; ++y) {
+            for (int x = start_x; x < SIZE_X; ++x) {
+                uint8_t voxel = array[x][y][z];
+                threshold_array[x][y][z] = (voxel > THRESHOLD_LIMIT) ? 1 : 0;
+            }
+        }
+    }
+
+    // Stop sub-timing
+    clock_t end_time = clock();
+    double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
+    printf(" - Time taken to threshold section %d: %f seconds\n", i, elapsed_time);
+	pthread_exit(NULL);
+}
+
+void *thread_reducing(void *i) {
+    // Start sub-timing
+    clock_t start_time = clock();
+
+    const int thread_index = (int)i;
+
+	printf("* Reducing in section: %d\n", i);
+
+    reduce(array, reduced_array, thread_index);
+
+    // Stop sub-timing
+    clock_t end_time = clock();
+    double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
+    printf(" - Time taken to reduce section %d: %f seconds\n", i, elapsed_time);
+	pthread_exit(NULL);
+}
+
+int main() {
     threshold_array = (uint8_t***) malloc(SIZE_X * sizeof(uint8_t**));
 
     if(threshold_array == NULL) {
@@ -138,7 +217,6 @@ int main() {
 
     /* Initializing Image array */
 
-    uint8_t*** array;
     array = (uint8_t***) malloc(SIZE_X * sizeof(uint8_t**));
 
     if(array == NULL) {
@@ -194,14 +272,9 @@ int main() {
 
     printf("* Thresholding the voxels...\n");
 
-    // Thresholding voxels
-    for (int z = 0; z < SIZE_Z; ++z) {
-        for (int y = 0; y < SIZE_Y; ++y) {
-            for (int x = 0; x < SIZE_X; ++x) {
-                uint8_t voxel = array[x][y][z];
-                threshold_array[x][y][z] = (voxel > THRESHOLD_LIMIT) ? 1 : 0;
-            }
-        }
+    for(int i=0; i< MAX_THREAD; i++) {
+        pthread_create(&threads[i], NULL, thread_thresholding, (void *)i);
+        pthread_join(threads[i], NULL);
     }
 
     printf("* Thresholding of voxels finished !\n");
@@ -211,12 +284,15 @@ int main() {
     printf("* Reducing the array...\n");
 
     unsigned int size = (SIZE_X * SIZE_Y * SIZE_Z) / (PATTERN_SIZE * PATTERN_SIZE * PATTERN_SIZE);
-    uint64_t* reduced_array = malloc(sizeof(uint64_t) * size);
+    reduced_array = malloc(sizeof(uint64_t) * size);
     if(reduced_array == NULL) {
         perror("Error creating array");
     }
 
-    reduce(threshold_array, reduced_array);
+    for(int i=0; i< MAX_THREAD; i++) {
+        pthread_create(&threads[i], NULL, thread_reducing, (void *)i);
+        pthread_join(threads[i], NULL);
+    }
 
     printf("* Finished reducing the array !\n");
 
